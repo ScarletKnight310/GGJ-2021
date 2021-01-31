eb44a07583840982e1a15c45de8d0b323e35c4eb
Smackledorf <delsonj44@gmail.com>
2021-01-31 12:05:54 -0800

    camera and derez

---
+++ Assets/AdventureLevel/Scripts/CameraFollow.cs
@@ -0,0 +1,15 @@
+using System;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform target;
+    
+    public float distDampening;
+
+    private void FixedUpdate()
+    {
+        transform.position = new Vector3( Mathf.Lerp(transform.position.x, target.position.x, Time.deltaTime * distDampening), Mathf.Lerp(transform.position.y, target.position.y, Time.deltaTime * distDampening), transform.position.z );
+        
+    }
+}
